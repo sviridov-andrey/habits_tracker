202f0550752d7dc25af09fe64461829ddcd0ead7
Андрей <chosenfew@yandex.ru>
2024-01-12 21:33:37 +0300

    модель Habit зарегистрирована в админке

--- habit/admin.py
+++ habit/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from habit.models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    """ Привычки в админке """
+
+    list_display = ('name', 'habit_is_public', 'user',)
+    list_filter = ('name',)
+    search_fields = ('name',)
