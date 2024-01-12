9371d59b1bc09d3b3023d6710220e98bd2b626b5
Андрей <chosenfew@yandex.ru>
2024-01-12 21:42:10 +0300

    созданы контроллеры для мдели Habit

--- habit/views.py
+++ habit/views.py
@@ -1,3 +1,82 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from habit.models import Habit
+from habit.serializers import HabitSerializer
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    """ Создание привычки """
+
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+
+        new_habit = serializer.save()
+        new_habit.user = self.request.user
+     #   create_reminder(new_habit)
+        new_habit.save()
+
+
+class HabitListAPIView(generics.ListAPIView):
+    """ Вывод списка привычек пользователя """
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+   # pagination_class = HabitPaginator
+    permission_classes = [IsAuthenticated, # IsOwner
+                          ]
+
+    def get_queryset(self):
+
+        queryset = Habit.objects.filter(user=self.request.user)
+        return queryset
+
+
+class HabitPublicListAPIView(generics.ListAPIView):
+    """ Вывод списка публичных привычек """
+
+    serializer_class = HabitSerializer
+   # pagination_class = HabitPaginator
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+
+        queryset = Habit.objects.filter(habit_is_public=True)
+        return queryset
+
+
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    """ Просмотр одной привычки """
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, #IsOwner
+                          ]
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    """ Изменение привычки """
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+  #  permission_classes = [IsAuthenticated, IsOwner]
+
+    def perform_update(self, serializer):
+
+        habit = serializer.save()
+     #   update_reminder(habit)
+        habit.save()
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    """ Удаление привычки """
+
+    queryset = Habit.objects.all()
+  #  permission_classes = [IsAuthenticated, IsOwner]
+
+    def perform_destroy(self, instance):
+
+   #     delete_reminder(instance)
+        instance.delete()
