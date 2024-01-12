1e54df39a44aaf54fca65d3543b95ae1fc620936
Андрей <chosenfew@yandex.ru>
2024-01-12 18:11:53 +0300

    созданы пути для модели User

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,19 @@
+from django.urls import path
+
 from users.apps import UsersConfig
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from users.views import UserListAPIView, UsersRegistrationView, UserRetrieveAPIView, UserUpdateAPIView, \
+    UserDestroyAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', UserListAPIView.as_view(), name='user_list'),
+    path('user_register/', UsersRegistrationView.as_view(), name='user_register'),
+    path('user/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_detail'),
+    path('user/update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_change'),
+    path('user/delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user_delete'),
 
+    path('token/', TokenObtainPairView.as_view(), name='token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
