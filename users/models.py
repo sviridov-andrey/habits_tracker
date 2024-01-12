6ebcb35fbbfb4dc864ae4fee15c3ef04fc59a21f
Андрей <chosenfew@yandex.ru>
2024-01-12 15:27:56 +0300

    создан сериализатор UserRegisterSerializer

--- users/models.py
+++ users/models.py
@@ -7,10 +7,8 @@ from users.utils import NULLABLE
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='email')
-    phone = models.CharField(max_length=20, verbose_name='телефон')
-    city = models.CharField(max_length=50, verbose_name='город')
-    country = models.CharField(max_length=50, verbose_name='страна')
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    telegram_id = models.CharField(max_length=20, verbose_name='telegram_id', **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
--- users/serializers.py
+++ users/serializers.py
@@ -10,26 +10,28 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-# class UserRegisterSerializer(serializers.ModelSerializer):
-#     password2 = serializers.CharField()
-#
-#     class Meta:
-#         model = User
-#         fields = ['email', 'password', 'password2', 'telegram_id']
+class UserRegisterSerializer(serializers.ModelSerializer):
+    """Сериализатор для регистрации модели User"""
 
-    # def save(self, *args, **kwargs):
-    #     user = User(
-    #         email=self.validated_data['email'],
-    #         telegram_id=self.validated_data['telegram_id'],
-    #         is_superuser=False,
-    #         is_staff=False,
-    #         is_active=True
-    #     )
-    #
-    #     password = self.validated_data['password']
-    #     password2 = self.validated_data['password2']
-    #     if password != password2:
-    #         raise serializers.ValidationError({password: "Пароль не совпадает"})
-    #     user.set_password(password)
-    #     user.save()
-    #     return user
+    password2 = serializers.CharField()
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'password2', 'telegram_id']
+
+    def save(self, *args, **kwargs):
+        user = User(
+            email=self.validated_data['email'],
+            telegram_id=self.validated_data['telegram_id'],
+            is_superuser=False,
+            is_staff=False,
+            is_active=True
+        )
+
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        if password != password2:
+            raise serializers.ValidationError({password: "Пароль не совпадает"})
+        user.set_password(password)
+        user.save()
+        return user
