14d1c7bef81e9a02d9e95cc2d438b1d9a87f5116
Андрей <chosenfew@yandex.ru>
2024-01-20 16:14:52 +0300

    настройка celery

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'users',
     'habit',
+    'django_celery_beat',
 
 ]
 
@@ -149,3 +150,18 @@ REST_FRAMEWORK = {
     )
 }
 
+
+# URL-адрес брокера сообщений
+CELERY_BROKER_URL = 'redis://localhost:6379'
+
+# URL-адрес брокера результатов, также Redis
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+# Часовой пояс для работы Celery
+CELERY_TIMEZONE = "Europe/Moscow"
+
+# Флаг отслеживания выполнения задач
+CELERY_TASK_TRACK_STARTED = True
+
+# Максимальное время на выполнение задачи
+CELERY_TASK_TIME_LIMIT = 30 * 60
