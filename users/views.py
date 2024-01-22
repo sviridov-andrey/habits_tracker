4eb03ffc2ffce30c5be71cb141b5a10b62f48ecf
sviridov-andrey <chosenfew@yandex.ru>
2024-01-22 16:30:32 +0300

    Исправления по PEP8

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from habit.permissions import IsOwner
 from users.models import User
 from users.serializers import UserSerializer, UserRegisterSerializer
 
@@ -25,14 +26,14 @@ class UserUpdateAPIView(generics.UpdateAPIView):
 
     serializer_class = UserSerializer
     queryset = User.objects.all()
-   # permission_classes = [IsAuthenticated, IsOwner]
+    permission_classes = [IsAuthenticated, IsOwner]
 
 
 class UserDestroyAPIView(generics.DestroyAPIView):
     """ Удаление пользователя """
 
     queryset = User.objects.all()
-   # permission_classes = [IsAuthenticated, IsOwner]
+    permission_classes = [IsAuthenticated, IsOwner]
 
 
 class UsersRegistrationView(generics.CreateAPIView):
